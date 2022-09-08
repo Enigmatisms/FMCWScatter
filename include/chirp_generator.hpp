fb0021918d65d9e2aa5d2362179dadfcb359f429
Enigmatisms <984041003@qq.com>
2022-09-08 20:08:09 +0800

    Chirp generator almost done

--- include/chirp_generator.hpp
+++ include/chirp_generator.hpp
@@ -5,44 +5,63 @@
 #include <random>
 #include <Eigen/Dense>
 
+template <typename T>
 class ChirpGenerator {
+using complex_t = std::complex<T>;
 public:
-    ChirpGenerator(float edge_len, float band_w, float sp_int, float tof_std, float sample_std):
-        edge_length(edge_len), band_width(band_w), sample_int(sp_int), tof_noise_std(tof_std), sample_noise_std(sample_std)
+    ChirpGenerator(T base_f, T edge_len, T band_w, T sp_int, T tof_std, T doppler_std, T sample_std):
+        base_freq(base_f), edge_length(edge_len), band_width(band_w), 
+        sample_int(sp_int), tof_noise_std(tof_std), doppler_noise_std(doppler_std), sample_noise_std(sample_std)
     {
-        int total_len = static_cast<int>(edge_length / sample_int) + 1;
+        total_len = static_cast<size_t>(edge_length / sample_int) + 1;
         pos_chirp.resize(total_len, 0.);
         neg_chirp.resize(total_len, 0.);
         // TODO: 对于发射信号而言，是否在构造时进行计算比较好？构造时计算正弦，在速度为0的时候也可以直接使用此正弦
         // 只在有速度（多普勒效应）时重新进行计算
     }
 public:
-    float set_delay_time(float d_time) {
+    T set_delay_time(T d_time) {        // should be called every evaluation
         // non-fixed random seed
         static std::default_random_engine engine(std::chrono::system_clock::now().time_since_epoch().count());
-        static std::normal_distribution<float> tof_noise(0.0, tof_noise_std);
+        static std::normal_distribution<T> tof_noise(0.0, tof_noise_std);
         delay_time = d_time + tof_noise(engine);
     }
 
     // 计算signal points，如果doppler_k大于0.0 则需要改变频率重新采样
-    void generateSignalPoints(float doppler_k = -1.);
-
-    // 在进行modulation时，由于Rx信号滞后于Tx信号，Tx信号的部分已经丢失，不参与计算，故Rx，Tx都需要剪切
-    void signalCropping(std::vector<float>& output, size_t number) const;
+    void generateSignalPoints(std::vector<T>& output, T doppler_mv = 0.0, bool perturb = false) const;
 
     // (1) tx，rx信号相乘（调制）（2）进行低通滤波（3）FFT得到频谱（4）频谱进行有意义的变化（FFT变为频率）
     // 输入 tx 信号，rx 信号，输出频谱(spect)以及拍频（beat_f）
-    void modulateThenTransform(const std::vector<float>& tx, const std::vector<float>& rx, std::vector<float>& spect, float& beat_f) const;
+    void modulateThenTransform(const std::vector<T>& tx, const std::vector<T>& rx, std::vector<T>& spect, T& beat_f, T cutoff_f) const;
 
-    void solve(float beat_pos, float beat_neg) const;
+    void solve(T beat_pos, T beat_neg) const;
+
+    // 在进行modulation时，由于Rx信号滞后于Tx信号，Tx信号的部分已经丢失，不参与计算，故Rx，Tx都需要剪切
+    static void signalCropping(const std::vector<T>& input, std::vector<T>& output, size_t number, bool front = true);
+
+    static void zeroPadding(std::vector<T>& inout) {
+        size_t len = inout.size();
+        if ((len & (len - 1)) == 0) {           // 如果长度已经是2的幂次，那么不需要进行padding
+            return;
+        }
+        size_t ub = static_cast<size_t>(std::ceil(log2(inout.size())));
+        inout.resize(pow(2, ub));
+    }
+
+    // static void amplitudeLowPass(T cutoff_f, T base_f, int rn = 4);
+
+    static complex_t freqButterworth4(int k, T cutoff_f, T base_f);
 private:
-    float edge_length;      // (Tc) 上升、下降沿的长度
-    float band_width;       // B
-    float sample_int;       // sampling interval
-    float delay_time;       // ToF
-    float tof_noise_std;    // noise of ToF (should be small)
-    float sample_noise_std; // noise add to each time sample (gaussian std) (之后需要给此值赋值)
+    T base_freq;            // Base frequency
+    T edge_length;          // (Tc) 上升、下降沿的长度
+    T band_width;           // B
+    T sample_int;           // sampling interval
+    T delay_time;           // ToF
+    T tof_noise_std;        // noise of ToF (should be small)
+    T doppler_noise_std;    // Doppler effect noise std
+    T sample_noise_std;     // noise add to each time sample (gaussian std) (之后需要给此值赋值)
 
-    std::vector<float> pos_chirp;
-    std::vector<float> neg_chirp;
+    size_t total_len;
+    std::vector<T> pos_chirp;
+    std::vector<T> neg_chirp;
 };
---
+++ src/chirp_generator.cc
@@ -0,0 +1,77 @@
+#include <algorithm>
+#include "simple_fft/fft.h"
+#include "chirp_generator.hpp"
+
+constexpr float PI = 3.141592653589793;
+
+// 计算signal points，如果doppler_k大于0.0 则需要改变频率重新采样
+template <typename T>
+void ChirpGenerator<T>::generateSignalPoints(std::vector<T>& output, T doppler_mv, bool perturb) const {
+    static std::default_random_engine engine(std::chrono::system_clock::now().time_since_epoch().count());
+    std::normal_distribution<T> doppler_noise(0.0, tof_noise_std);
+    doppler_mv += doppler_noise(engine);
+    auto sinusoid = [b_f = base_freq, B = band_width, t = edge_length, dt = sample_int, doppler_mv](size_t sp_i) {
+        T now_t = dt * static_cast<T>(sp_i);
+        return sin((b_f + B / T * now_t + doppler_mv) * now_t);
+    };
+    output.resize(total_len);
+    std::generate(output.begin(), output.end(), [n = 0] () mutable {return n++;});
+    std::transform(output.begin(), output.end(), output.begin(), sinusoid);
+    if (perturb == true) {
+        std::normal_distribution<T> sample_noise(0.0, sample_noise_std);
+        std::transform(output.begin(), output.end(), output.begin(), [&sample_noise, &engine](T val) {return val + sample_noise(engine);});
+    }
+}
+
+// 在进行modulation时，由于Rx信号滞后于Tx信号，Tx信号的部分已经丢失，不参与计算，故Rx，Tx都需要剪切
+template <typename T>
+void ChirpGenerator<T>::signalCropping(const std::vector<T>& input, std::vector<T>& output, size_t number, bool front) {
+    if (front == true) {
+        output.assign(input.begin() + number, input.end());
+    } else {
+        output.assign(input.begin(), input.end() - number);
+    }
+}
+
+// (1) tx，rx信号相乘（调制）（2）进行低通滤波（3）FFT得到频谱（4）频谱进行有意义的变化（FFT变为频率）
+// 输入 tx 信号，rx 信号，输出频谱(spect)以及拍频（beat_f）
+template <typename T>
+void ChirpGenerator<T>::modulateThenTransform(const std::vector<T>& tx, const std::vector<T>& rx, std::vector<T>& spect, T& beat_f, T cutoff_f) const {
+    const char* error_description = nullptr;
+    std::vector<T> modulated(tx.size());
+    std::vector<complex_t> outputs;
+
+    std::transform(tx.begin(), tx.end(), rx.begin(), modulated.begin(), [](T v1, T v2){return v1 * v2;});
+    simple_fft::FFT(modulated, outputs, modulated.size(), error_description);
+
+    if (cutoff_f > 1e-9) {
+        const T base_f = 2 * PI / static_cast<T>(tx.size());
+        std::vector<complex_t> butterworth_lp(modulated.size());
+        std::generate(butterworth_lp.begin(), butterworth_lp.end(), 
+            [k = 0, cutoff_f, base_f]() mutable {
+                return freqButterworth4(k, cutoff_f, base_f);
+            }
+        );
+        std::transform(outputs.begin(), outputs.end(), butterworth_lp.begin(), outputs.begin(), [](const auto& v1, const auto& v2){return v1 * v2;});
+    }
+    // TODO: 有意义的变换
+}
+
+template <typename T>
+void ChirpGenerator<T>::solve(T beat_pos, T beat_neg) const {
+
+}
+
+template <typename T>
+void ChirpGenerator<T>::zeroPadding(std::vector<T>& inout) {
+    size_t ub = static_cast<size_t>(std::ceil(log2(inout.size())));
+    inout.resize(pow(2, ub));
+}
+
+template <typename T>
+ChirpGenerator<T>::complex_t ChirpGenerator<T>::freqButterworth4(int k, T cutoff_f, T base_f) {
+    const complex_t s(0., base_f * static_cast<T>(k) / cutoff_f);
+    const complex_t s2 = s * s;   
+    return 1. / (s2 + 0.765367 * s + 1) / (s2 + 1.847759 * s + 1);
+}
+
