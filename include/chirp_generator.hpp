4aa7aed6190b2564ef140a6f9b30de305b44e57a
Enigmatisms <984041003@qq.com>
2022-09-08 21:56:23 +0800

    Butterworth problem resolved

--- include/chirp_generator.hpp
+++ include/chirp_generator.hpp
@@ -20,6 +20,8 @@ public:
         // 只在有速度（多普勒效应）时重新进行计算
     }
 public:
+    void sendOneFrame(T gt_depth);      // simulation
+private:
     T set_delay_time(T d_time) {        // should be called every evaluation
         // non-fixed random seed
         static std::default_random_engine engine(std::chrono::system_clock::now().time_since_epoch().count());
@@ -34,7 +36,10 @@ public:
     // 输入 tx 信号，rx 信号，输出频谱(spect)以及拍频（beat_f）
     void modulateThenTransform(const std::vector<T>& tx, const std::vector<T>& rx, std::vector<T>& spect, T& beat_f, T cutoff_f) const;
 
-    void solve(T beat_pos, T beat_neg) const;
+    void solve(T beat_pos, T beat_neg, T& range, T& speed) const {
+        range = c_vel * edge_length / (8. * band_width) * (beat_pos + beat_neg);
+        speed = wave_length / 4. * (beat_neg - beat_pos);
+    }
 
     // 在进行modulation时，由于Rx信号滞后于Tx信号，Tx信号的部分已经丢失，不参与计算，故Rx，Tx都需要剪切
     static void signalCropping(const std::vector<T>& input, std::vector<T>& output, size_t number, bool front = true);
@@ -48,8 +53,6 @@ public:
         inout.resize(pow(2, ub));
     }
 
-    // static void amplitudeLowPass(T cutoff_f, T base_f, int rn = 4);
-
     static complex_t freqButterworth4(int k, T cutoff_f, T base_f);
 private:
     T base_freq;            // Base frequency
@@ -60,6 +63,7 @@ private:
     T tof_noise_std;        // noise of ToF (should be small)
     T doppler_noise_std;    // Doppler effect noise std
     T sample_noise_std;     // noise add to each time sample (gaussian std) (之后需要给此值赋值)
+    T wave_length = 1550e-9;// wave length of LiDAR (1550 nm)
 
     size_t total_len;
     std::vector<T> pos_chirp;
--- src/chirp_generator.cc
+++ src/chirp_generator.cc
@@ -3,6 +3,12 @@
 #include "chirp_generator.hpp"
 
 constexpr float PI = 3.141592653589793;
+constexpr float c_vel = 299792458;  // speed of light
+
+template <typename T>
+size_t arg_max(const std::vector<T>& vec) {
+    return static_cast<size_t>(std::distance(vec.begin(), std::max_element(vec.begin(), vec.end())));
+}
 
 // 计算signal points，如果doppler_k大于0.0 则需要改变频率重新采样
 template <typename T>
@@ -38,28 +44,32 @@ void ChirpGenerator<T>::signalCropping(const std::vector<T>& input, std::vector<
 template <typename T>
 void ChirpGenerator<T>::modulateThenTransform(const std::vector<T>& tx, const std::vector<T>& rx, std::vector<T>& spect, T& beat_f, T cutoff_f) const {
     const char* error_description = nullptr;
+    const T base_f = 2 * PI / static_cast<T>(tx.size());
     std::vector<T> modulated(tx.size());
     std::vector<complex_t> outputs;
 
     std::transform(tx.begin(), tx.end(), rx.begin(), modulated.begin(), [](T v1, T v2){return v1 * v2;});
+    zeroPadding(modulated);
+    size_t signal_len = modulated.size();
     simple_fft::FFT(modulated, outputs, modulated.size(), error_description);
 
     if (cutoff_f > 1e-9) {
-        const T base_f = 2 * PI / static_cast<T>(tx.size());
         std::vector<complex_t> butterworth_lp(modulated.size());
         std::generate(butterworth_lp.begin(), butterworth_lp.end(), 
             [k = 0, cutoff_f, base_f]() mutable {
-                return freqButterworth4(k, cutoff_f, base_f);
+                return freqButterworth4(k++, cutoff_f, base_f);
             }
         );
         std::transform(outputs.begin(), outputs.end(), butterworth_lp.begin(), outputs.begin(), [](const auto& v1, const auto& v2){return v1 * v2;});
     }
+    outputs.resize(1 + (signal_len >> 1));
+
+    auto get_amp_func = [](const complex_t& c) {return sqrt(pow(c.real(), 2) + pow(c.imag(), 2));};
+    spect.resize(signal_len);
+    std::transform(outputs.begin(), outputs.end(), spect.begin(), get_amp_func);
+    size_t k_num = arg_max(spect);
+    beat_f = base_f * static_cast<T>(k_num);
     // TODO: 有意义的变换
-}
-
-template <typename T>
-void ChirpGenerator<T>::solve(T beat_pos, T beat_neg) const {
-
 }
 
 template <typename T>
@@ -74,4 +84,3 @@ ChirpGenerator<T>::complex_t ChirpGenerator<T>::freqButterworth4(int k, T cutoff
     const complex_t s2 = s * s;   
     return 1. / (s2 + 0.765367 * s + 1) / (s2 + 1.847759 * s + 1);
 }
-
